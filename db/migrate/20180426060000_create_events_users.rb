2d809cff1457cb96a0eb8d5d489a3a6dbca1f197
Nicholas Greeby <greeby@seas.upenn.edu>
2018-04-26 12:28:20 -0400

    Heroku gems

--- db/migrate/20180426060000_create_events_users.rb
+++ db/migrate/20180426060000_create_events_users.rb
@@ -1,4 +1,5 @@
 class CreateEventsUsers < ActiveRecord::Migration[5.1]
+  def change
     create_table :events_users do |t|
       t.references :event, foreign_key: true
       t.references :user, foreign_key: true
